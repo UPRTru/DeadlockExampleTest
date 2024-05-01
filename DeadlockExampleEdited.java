4958378897be04a79239bbb56677f579e5b6835d
UPRTru <108067330+UPRTru@users.noreply.github.com>
2024-05-01 16:55:54 +0300

    Update DeadlockExampleEdited.java
    Edit acquireResourcesAndWork void. firstLock.unlock();

--- DeadlockExampleEdited.java
+++ DeadlockExampleEdited.java
@@ -1,6 +1,10 @@
 package edited;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DeadlockExampleEdited {
 
@@ -30,34 +34,46 @@ public class DeadlockExampleEdited {
         thread2.start();
     }
 
-    private synchronized void acquireResourcesAndWork(ReentrantLock firstLock, ReentrantLock secondLock, Resource firstResource, Resource secondResource, String threadName) {
-        firstLock.lock();
-        System.out.println(threadName + " locked " + firstResource);
-
+    @SuppressWarnings("empty-statement")
+    private void acquireResourcesAndWork(Lock firstLock, Lock secondLock, Resource firstResource, Resource secondResource, String threadName) {
+        Resource oldFirstResource = firstResource;
         try {
-            // Имитация работы с ресурсом
-            Thread.sleep(100);
-
-            secondLock.lock();
-            System.out.println(threadName + " locked " + secondResource);
-
+            while (!firstLock.tryLock(1, TimeUnit.SECONDS));
+            System.out.println(threadName + " locked " + firstResource);
+            
             try {
                 // Имитация работы с ресурсом
                 Thread.sleep(100);
+                
+                while (((ReentrantLock) secondLock).isLocked()) {
+                    firstResource = oldFirstResource;
+                    firstLock.unlock();
+                    System.out.println("Метод: " + threadName + " ожидает ресурс 2. "
+                            + "\nОсвобождение ресурса 1.");
+                    while (!firstLock.tryLock(1, TimeUnit.SECONDS));
+                    System.out.println(threadName + " locked " + firstResource);
+                }
+                
+                while (!secondLock.tryLock(1, TimeUnit.SECONDS));
+                System.out.println(threadName + " locked " + secondResource);
+                
+                try {
+                    // Имитация работы с ресурсом
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    secondLock.unlock();
+                    System.out.println(threadName + " unlocked " + secondResource);
+                }
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             } finally {
-                if (firstLock.isHeldByCurrentThread()) {
-                    firstLock.unlock();
-                }
+                firstLock.unlock();
                 System.out.println(threadName + " unlocked " + firstResource);
-                if (secondLock.isHeldByCurrentThread()) {
-                    secondLock.unlock();
-                }
-                System.out.println(threadName + " unlocked " + secondResource);
             }
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(DeadlockExample.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
